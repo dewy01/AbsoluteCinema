3d93b21e1f161904caa2d5860bf88e30872ff403
Dawid <dawid1234x@wp.pl>
2025-05-10 14:29:32 +0200

    feat: scaffold

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,10 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function App() {
-  return <div>Absolute cinema</div>
+  return <div>Absolute cinema</div>;
 }
 
-export default App
+export default App;
