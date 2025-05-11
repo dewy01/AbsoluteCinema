6a3a6d7ef374848a29540fea561b8734dd29ee8e
c0ndii <105825543+c0ndii@users.noreply.github.com>
2025-05-11 15:17:53 +0200

    Router init

---
+++ web/src/routes.ts
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Router>
+      <Routes></Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
